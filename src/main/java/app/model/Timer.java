41d25ea619fc147a1db9b0ab6ef7fe2eaf77cfe6
Yegor Yaroslavtsev <yaroslavtsevegor@gmail.com>
2022-07-13 11:36:37 +0500

    First attempt

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
-
-    private Long nanoTime = System.nanoTime();
-
+   private Long nanoTime = System.nanoTime();
     public Long getTime() {
         return nanoTime;
     }
